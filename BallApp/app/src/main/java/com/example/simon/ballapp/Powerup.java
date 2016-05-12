756ef6539f56292725d5b7079ed7bfaecd5b57fd
Patrick Q Jensen <patrickq116@hotmail.com>
2016-05-12 11:58:48 +0200

    Powerup class

---
+++ BallApp/app/src/main/java/com/example/simon/ballapp/Powerup.java
@@ -0,0 +1,43 @@
+package com.example.simon.ballapp;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Patrick Q Jensen on 12-05-2016.
+ */
+public class Powerup extends GameObject {
+
+    DisplayMetrics metrics;
+    int scrWidth, scrHeight;
+    float speedY, speedX;
+    float radius;
+
+    public Powerup(Context context, float left, float top, float right, float bottom) {
+        super(context, left, top, right, bottom);
+
+        paint.setColor(0xFF00FF00);
+
+        metrics = Resources.getSystem().getDisplayMetrics();
+        scrHeight = metrics.heightPixels;
+        scrWidth = metrics.widthPixels;
+        speedX = 0;
+        speedY = -10;
+        radius = objRect.right - objRect.left;
+    }
+
+    @Override
+    public void update()
+    {
+        super.update();
+
+        Move();
+    }
+
+    public void Move()
+    {
+        y  += speedY;
+        x += speedX;
+    }
+}
